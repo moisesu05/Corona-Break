7bd1c8e0010bfd52fabef36504abe1bd16a329dd
moisesu05 <moisesronanl@gmail.com>
2020-05-05 13:46:17 +0800

    1st Commit

---
+++ Corona Break/Assets/Scripts/BasicMovement.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicMovement : MonoBehaviour
+{
+    //  public Animator animator;
+ //   public float movespeed = 5;
+    // Update is called once per frame
+    void Update()
+    {
+       // animator.SetFloat("Horizontal", Input.GetAxis("Horizontal"));
+
+        float x = Input.GetAxis ("Horizontal") * Time.deltaTime * 40;
+        float y = Input.GetAxis ("Vertical") * Time.deltaTime * 40;
+
+        transform.Translate (x , y, 0);
+    }
+}
